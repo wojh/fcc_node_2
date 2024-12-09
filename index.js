f834725f9bd86f5b60768b1735806142c6aa4c98
jhow <desenvolvimentopessoal007@gmail.com>
2024-12-09 10:37:41 -0300

    finalizado

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res, next) => {
+  ip = req.ip;
+  lang = req.headers['accept-language'];
+  softw = req.headers['user-agent'];
+  next();
+}, (req, res) => {
+  res.json({ipaddress: ip, language: lang, software: softw});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
